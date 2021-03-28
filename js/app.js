b15e11a4ff181f7622fccaaf411e8c483194a0f3
johwid <>
2021-03-28 22:02:15 +0200

    Fixed invalid character.

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ function newQuestion(words){
     
     // Get answers
     var answers=[]
-    var numberOfAnswers=10§
+    var numberOfAnswers=10
 
     // Push correct answer to the list
     answers.push([from,to]);
